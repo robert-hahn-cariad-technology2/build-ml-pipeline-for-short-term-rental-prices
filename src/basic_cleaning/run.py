2130d439b991bd3952816c5c4e06510e401d923e
Robert Hahn <robert.hahn@cariad.technology>
2026-06-09 13:55:34 +0200

    step 5 fixes

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -21,9 +21,15 @@ def go(args):
 
     local_path = run.use_artifact(args.input_artifact).file()
     df = pd.read_csv(local_path)
-
+    #price range limitations
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
+
+    #positional limitations
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
+    logger.info("Cleaned data has %s rows and %s columns", *df.shape)
 
     df.to_csv("clean_sample.csv", index=False)
 
